c57162fc1aefe14b73dfa8b73f0fbb5c6997ecfc
Christian Osorio <christiandoor.2001@gmail.com>
2023-05-29 00:14:19 -0400

    fix/ Configurado archivo de entrada main.php

--- main.php
+++ main.php
@@ -1,21 +1,21 @@
 <?php
 
 /**
- * Plugin Name:       Boilerplate
- * Description:       Write a description for block
+ * Plugin Name:       Bloques Forma
+ * Description:       Seria de bloques personalizados por Forma para mejorar la funcionalidad y el diseño de tu sitio web
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            Boilerplate
+ * Author:            Forma
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       boilerplate
+ * Text Domain:       blocksforma
  *
- * @package           create-block
+ * @package           blocksforma
  */
 
-function create_block_boilerplate_block_init()
+function init_forma_blocks()
 {
 	register_block_type(__DIR__ . '/build');
 }
-add_action('init', 'create_block_boilerplate_block_init');
+add_action('init', 'init_forma_blocks');
