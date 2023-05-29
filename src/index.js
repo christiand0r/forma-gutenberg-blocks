4bacedf50a0bb63d8cd02face5d08e9a794d9cb4
Christian Osorio <christiandoor.2001@gmail.com>
2023-05-29 00:15:34 -0400

    fix/ Implentado modelo de importación para trabajar con diversos bloques

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
-import { registerBlockType } from '@wordpress/blocks';
-import './style.scss';
-
-import Edit from './edit';
-import save from './save';
-import metadata from './block.json';
-
-registerBlockType( metadata.name, {
-	edit: Edit,
-	save,
-} );
+// @Blocks
+import './blocks/Tabs';
